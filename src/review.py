7ce52c30ddf95212351f97e90ebb9047065d8f5d
TFrankeM <105611304+TFrankeM@users.noreply.github.com>
2024-10-12 17:59:09 -0300

    Atualiza a classe review

--- src/review.py
+++ src/review.py
@@ -3,13 +3,42 @@ import uuid
 
 
 class Review:
+    """
+    Class to represent a review made by a user for a product or vending machine.
+    """
 
     def __init__(self, user_id, recipient_id, rating, comment=None):
-
+        """
+        Initialize the review with id, user_id, recipient_id, rating, and an optional comment.
+        
+        Parameters:
+        -----------
+        user_id (int): ID of the user who made the review.
+        recipient_id (int): ID of the recipient being reviewed.
+        rating (int): Rating provided by the user (0 to 5).
+        comment (str, optional): Optional comment provided by the user (default is None).
+        """
         self.id = uuid.uuid4()
         self.date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         self.comment = comment
         self.rating = rating
         self.user_id = user_id
         self.recipient_id = recipient_id
+
+
+    def __str__(self):
+        """
+        Return a string representation of the review.
+        """
+        return f"Review by User {self.user_id} for {self.recipient_id}: {self.rating}/5 - {self.comment}"
+
+
+    def validate_rating(self):
+        """
+        Validate if the rating is between 0 and 5.
+        """
+        if 0 <= self.rating <= 5:
+            return True
+        else:
+            return False
 
--- test/test_review.py
+++ test/test_review.py
@@ -30,7 +30,28 @@ class TestReview(unittest.TestCase):
         self.assertEqual(self.review.rating, 4)
         self.assertEqual(self.review.comment, "Good product!")
         self.assertIsNotNone(self.review.date)  # A data deve ser gerada automaticamente
+    
 
+    def test_validate_rating(self):
+        """
+        Testa a função de validação de nota (rating). Deve retornar True para notas entre 0 e 5.
+        """
+        valid_review = Review(user_id=102, recipient_id=203, rating=5)
+        invalid_review = Review(user_id=103, recipient_id=204, rating=6)
+        
+        self.assertTrue(valid_review.validate_rating())  # Nota válida
+        self.assertFalse(invalid_review.validate_rating())  # Nota inválida
+    
+
+    def test_str_method(self):
+        """
+        Testa se o método __str__ retorna a string correta.
+        """
+        # Exemplo de avaliação para uso nos testes
+        self.review = Review(user_id=101, recipient_id=202, rating=4, comment="Good product!")
+        
+        expected_str = "Review by User 101 for 202: 4/5 - Good product!"
+        self.assertEqual(str(self.review), expected_str)
 
 
 # Executar os testes
